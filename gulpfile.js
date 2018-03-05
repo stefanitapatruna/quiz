942c55a2f3eff643e7f824ac7ed99b9267bf3cbe
stefanitapatruna <stefanita.patruna@gmail.com>
2018-03-05 18:47:22 +0200

    Browser sync added to gulfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,64 @@
-'use-strict';
 
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 var livereload = require('gulp-livereload');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 gulp.task('scripts', function() {
     gulp.src(['app/**/*.js'])
         .pipe(gulp.dest('public'))
+        .pipe(reload({
+                stream: true
+            }))
 });
 
-gulp.task('modules', function() {
-    gulp.src(['app/**/**/**/*.js'])
-        .pipe(gulp.dest('public'))
-});
 
 gulp.task('scss', function() {
     gulp.src(['app/**/*.scss'])
     	.pipe(sass())
         .pipe(gulp.dest('public'))
+        .pipe(reload({
+                stream: true
+            }))
 });
 
 gulp.task('html', function() {
-    gulp.src(['app/*.html'])
+    gulp.src('app/*.html')
     .pipe(gulp.dest('public'))
+    .pipe(reload({
+                stream: true
+            }))
     });
 
 gulp.task('html2', function() {
-    gulp.src(['app/**/*.html'])
+    gulp.src('app/**/*.html')
     .pipe(gulp.dest('public'))
+    .pipe(reload({
+                stream: true
+            }))
     });
+
+gulp.task('browser-sync', function() {
+
+    browserSync({
+        notify: false,
+        server: {
+            baseDir: 'public'
+        }
+    });
+
+});
+
 
 gulp.task('watch', function() {
   
   //livereload.listen();
   gulp.watch('app/**/*.scss', ['scss']);
   gulp.watch('app/**/*.js', ['scripts']);
-  gulp.watch('app/**/**/**/*.js', ['modules']);
   gulp.watch('app/*.html', ['html']);
   gulp.watch('app/**/*.html', ['html2']);
 });
 
-gulp.task('default',['html','html2','scripts','modules','scss','watch']);
+gulp.task('default',['html','html2','scripts','scss','watch','browser-sync']);
