df9465d6be51b85cb4004fae225f83ebdfa02f17
ESP\stefanita.patruna <stefanita.patruna@gmail.com>
2018-03-09 17:44:58 +0200

    ui router configured

--- public/javascript/modules/base/baseController.js
+++ public/javascript/modules/base/baseController.js
@@ -2,20 +2,39 @@ angular.module('baseModule',['ui.router'])
 	.controller('baseModuleController',['$scope', function($scope){
 		$scope.myVariable= "something from angular";
 
+		$scope.expandedTabs = {
+								'home': true,
+								'login': false,
+								'userCP': false,
+								'products': false,
+								'search': false
+							};
+		
+		$scope.toggleTab = function(tabName){
+			
+		}						
+
 	}])
 	.config(['$stateProvider', function($stateProvider , $urlRouterProvider){
 		$stateProvider
-		.state('homepage',{
-			url:'/homepage',
-			templateUrl:'',
-			controller: 'homepageController'
+		.state('aboutUs',{
+			url:'/aboutUs',
+			templateUrl:'html/pages/aboutUs.html'
+		})
+		.state('whatWeDo',{
+			url:'/whatWeDo',
+			templateUrl: 'html/pages/whatWeDo.html'
+		})
+		.state('contactUs',{
+			url:'/contactUs',
+			templateUrl: 'html/pages/contactUs.html'
 		})
 		.state('root',{
 			url:'/',
-			templateUrl:'../../../html/pages/home.html',
+			templateUrl:'html/pages/aboutUs.html',
 		})
 		.state('otherwise',{
 			url:'*path',
-			templateUrl:'../../../html/pages/home.html'
+			templateUrl:'html/pages/aboutUs.html'
 		})
 	}]);
