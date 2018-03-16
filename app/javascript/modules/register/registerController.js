a786a48088afa018f48f7b039cadd9313d1db2d7
ESP\stefanita.patruna <stefanita.patruna@gmail.com>
2018-03-16 17:09:48 +0200

    register form

--- app/javascript/modules/register/registerController.js
+++ app/javascript/modules/register/registerController.js
@@ -1,6 +1,20 @@
 angular.module('registerModule',[])
 	.controller('registerController',['$scope', function($scope){
 
-		$scope.registerControllerVariable = 'this is a variable from register controller'; 
-		
+		$scope.minAge = 18; 
+		$scope.maxAge = 99;
+
+		$scope.userData = {
+			fname: '',
+			lname: '',
+			password: '',
+			retypePass: '',
+			gender: '',
+			age: '',
+			aboutYou: ''
+		};
+
+		$scope.registerUser = function(){
+			console.log('user registered', this.userData);
+		}
 	}]);
